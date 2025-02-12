3e4d35658338a51a49335fa5875ce34f42f8f3f9
Ekpereka Amutaigwe <uzoamutaigwe@yahoo.com>
2025-02-12 02:09:48 -0800

    complete stage zero task 1

---
+++ stage-zero/leucine_gp_task1.py
@@ -0,0 +1,34 @@
+# Use any data structure of your choice to organize the following information
+# Your names
+# Your slack username
+# Your email
+# Your hobby
+# Your countries
+# Your discipline
+# Your preferred programming language
+
+# Create a dictionary of dictionaries containing the information of the members of your group
+leucine_group_dict = {
+    # First Member 
+    "Uzo": {
+          "Full Name": "Uzo Amutaigwe",
+          "Slack Username": "Uzo",
+          "Email Address": "uzoamutaigwe@gmail.com",
+          "Hobby": "Singing and Dancing Alone",
+          "Country": "Canada",
+          "Discipline": "Genomics",
+          "Preferred programming language": "Python"
+    }
+}
+
+print("LEUCINE GROUP TEAMMATES' INFORMATION")
+print("\n")
+      
+print("First Member:")
+print("Full Name:", leucine_group_dict["Uzo"]["Full Name"])
+print("Slack Username:", leucine_group_dict["Uzo"]["Slack Username"])
+print("Email Address:", leucine_group_dict["Uzo"]["Email Address"])
+print("Hobby:", leucine_group_dict["Uzo"]["Hobby"])
+print("Country:", leucine_group_dict["Uzo"]["Country"])
+print("Discipline:", leucine_group_dict["Uzo"]["Discipline"])
+print("Preferred programming language:", leucine_group_dict["Uzo"]["Preferred programming language"])
